05870bcb86b0e0cbea6ef7f418f58aae914d7a7f
yinglm <yinglm@vip.qq.com>
2022-08-14 21:05:40 +0800

    第二次提交  完成根据根据四个方向移动 设置四个方向dir Test_tank

--- src/com/yingliming/com/yingliming/tank/TankFrame.java
+++ src/com/yingliming/com/yingliming/tank/TankFrame.java
@@ -15,6 +15,9 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     int x=200,y=200;
+    Dir dir= Dir.DOWN;
+    private final int SPEED=10;
+
     public TankFrame(){
         setSize(800,600);
         setResizable(false);
@@ -36,37 +39,79 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         g.fillRect(x,y,50,50);
-        /*x+=10;
-        y+=10;*/
+        switch (dir)
+        {
+            case LEFT: x-=SPEED;
+                break;
+            case RIGHT: x+=SPEED;
+                break;
+            case UP: y-=SPEED;
+                break;
+            case DOWN: y+=SPEED;
+                break;
+            default:
+                break;
+        }
+
     }
 
     private class MyKeyAdapter extends KeyAdapter {
+        boolean bl=false;
+        boolean br=false;
+        boolean bu=false;
+        boolean bd=false;
         @Override
         public void keyPressed(KeyEvent e) {
             //System.out.println("key press");
             int key = e.getKeyCode();
             switch (key)
             {
-                case KeyEvent.VK_LEFT: x-=10;
+                case KeyEvent.VK_LEFT: bl=true;
                     break;
-                case KeyEvent.VK_RIGHT: x+=10;
+                case KeyEvent.VK_RIGHT: br=true;
                     break;
-                case KeyEvent.VK_UP: y-=10;
+                case KeyEvent.VK_UP: bu=true;
                     break;
-                case KeyEvent.VK_DOWN: y+=10;
+                case KeyEvent.VK_DOWN: bd=true;
                     break;
                 default:
                     break;
             }
 
+            setMainTankDir();
+
 
 
         }
 
+
+
         @Override
         public void keyReleased(KeyEvent e) {
             //System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key)
+            {
+                case KeyEvent.VK_LEFT: bl=false;
+                    break;
+                case KeyEvent.VK_RIGHT: br=false;
+                    break;
+                case KeyEvent.VK_UP: bu=false;
+                    break;
+                case KeyEvent.VK_DOWN: bd=false;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
 
+        }
+
+        private void setMainTankDir() {
+            if(bl) dir=Dir.LEFT;
+            if(br) dir=Dir.RIGHT;
+            if(bu) dir=Dir.UP;
+            if(bd) dir=Dir.DOWN;
         }
     }
 }
