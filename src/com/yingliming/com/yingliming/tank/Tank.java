5a649998fd1e2c56633f7157db04179660bb45f2
v_win7_hc <yinglm@vip.qq.com>
2022-08-15 16:19:45 +0800

    20220815 提交    按下ctr 坦克fire出一堆子弹， 传递给TankFrame

--- src/com/yingliming/com/yingliming/tank/Tank.java
+++ src/com/yingliming/com/yingliming/tank/Tank.java
@@ -83,6 +83,6 @@ public class Tank {
     }
 
     public void fire() {
-        tf.bullet = new Bullet(x, y, dir);
+        tf.bullets.add(new Bullet(x,y,dir));
     }
 }
--- src/com/yingliming/com/yingliming/tank/TankFrame.java
+++ src/com/yingliming/com/yingliming/tank/TankFrame.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther: yingliming
@@ -15,7 +17,7 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     Tank myTank = new Tank(200,200,Dir.DOWN,this);
-    Bullet bullet = new Bullet(300,300,Dir.DOWN);
+    List<Bullet> bullets = new ArrayList<>();
     static final int GAME_WIDTH=800, GAME_HEIGHT=600;
 
     public TankFrame(){
@@ -57,9 +59,9 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
 
         myTank.paint(g);
-        bullet.paint(g);
-
-
+        for (Bullet b:bullets) {
+            b.paint(g);
+        }
     }
 
     private class MyKeyAdapter extends KeyAdapter {
